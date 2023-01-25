64c85828b2dd0cf76b6d82bda0c5fe10b63fd610
TadahiroYamamura <mt.village12@gmail.com>
2023-01-25 20:44:34 +0900

    NetRWの設定を一部移動させた

--- nvim/lua/netrw.lua
+++ nvim/lua/netrw.lua
@@ -4,10 +4,3 @@ vim.g.netrw_sizestyle = "H"
 vim.g.netrw_timefmt = "%Y/%m/%d(%a) %H:%M:%S"
 vim.g.netrw_fastbrowse = 0
 vim.g.netrw_preview = 1
-vim.api.nvim_create_autocmd(
-    "FileType",
-    {
-        pattern = "netrw",
-        callback = function() vim.b.bufhidden = "wipe" end,
-    }
-)
