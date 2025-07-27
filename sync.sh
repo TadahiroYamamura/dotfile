a33d0229add856eede427b1e69581f1e5f4e2da7
TadahiroYamamura <mt.village12@gmail.com>
2025-07-28 01:46:42 +0900

    sync.shを整備

--- sync.sh
+++ sync.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
 function nvim () {
-	cp -r $HOME/.config/nvim/* ${0%/*}/nvim/
+	rsync -a --delete $HOME/.config/nvim/ ${0%/*}/nvim/
 }
 
-nvim
+function fnc_exists () {
+	declare -F "$1" > /dev/null 2>&1
+	return $?
+}
+
+if [ "$#" = 0 ]; then
+	echo "sync.sh"
+	echo "========================="
+	echo "sync dotfiles."
+	echo
+	echo "[Usage]"
+	echo "sync.sh target [target...]"
+	echo
+	echo "[Available targets]"
+	echo "- nvim"
+else
+	for name in "$@"; do
+		if fnc_exists $name; then
+			$name
+		fi
+	done
+fi
