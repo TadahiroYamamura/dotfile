e4e480e4edf1fa4c78ca87081ea6f8b1474f7257
TadahiroYamamura <mt.village12@gmail.com>
2026-04-07 17:45:38 +0900

    keymapの定義方法をちょっと修正

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,9 +1,21 @@
 -- leaderをspaceに割当てる
 vim.g.mapleader = ' '
 
-vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<esc><esc>', ':nohl<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader><leader>', '<C-^>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-\\>', '<cmd>sp<cr><C-w>j<cmd>resize 5<cr><cmd>terminal<cr>i', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+local keymaps = {
+	n = {
+		["j"] = "gj",
+		["k"] = "gk",
+		["<esc><esc>"] = ":nohl<cr>",
+		["<leader><leader>"] = "<C-^>",
+		["<C-\\>"] = "<cmd>sp<cr><C-w>j<cmd>resize 5<cr><cmd>terminal<cr>i",
+	},
+	t = {
+		["<esc>"] = "<C-\\><C-n>",
+	},
+}
+
+for mode, keymap in pairs(keymaps) do
+	for key, command in pairs(keymap) do
+		vim.keymap.set(mode, key, command, { silent = true })
+	end
+end
