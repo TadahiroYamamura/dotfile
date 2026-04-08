b8f4da2416a91349c1dd2e55f5bfa2be24ed37d6
TadahiroYamamura <mt.village12@gmail.com>
2026-04-08 22:46:10 +0900

    LSPのキーバインド定義をkeymaps.luaに移動

--- nvim/lua/autocmd/lsp.lua
+++ nvim/lua/autocmd/lsp.lua
@@ -13,28 +13,3 @@ vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 	pattern = "*",
 	callback = vim.lsp.buf.clear_references,
 })
-
--- LSP全般で使いたいキーバインド
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local opts = { buffer = args.buf }
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'grr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'grn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', 'gra', vim.lsp.buf.code_action, opts)
-
-	-- 定義をジャンプせずにチラッと確認したい時に使う
-    vim.keymap.set("n", "gp", function()
-      local params = vim.lsp.util.make_position_params(0, 'utf-8')
-      return vim.lsp.buf_request(0, "textDocument/definition", params, function(_, result)
-        if result == nil or vim.tbl_isempty(result) then
-          print("No definition found")
-          return
-        end
-        vim.lsp.util.preview_location(result[1], { border = "rounded" })
-      end)
-    end, { desc = "Peek Definition" })
-
-  end,
-})
--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -19,3 +19,36 @@ for mode, keymap in pairs(keymaps) do
 		vim.keymap.set(mode, key, command, { silent = true })
 	end
 end
+
+-- LSP全般で使いたいキーバインド
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local keymaps = {
+      n = {
+        ["K"] = vim.lsp.buf.hover,
+        ["gd"] = vim.lsp.buf.definition,
+        ["grr"] = vim.lsp.buf.references,
+        ["grn"] = vim.lsp.buf.rename,
+        ["gra"] = vim.lsp.buf.code_action,
+      },
+    }
+
+    for mode, keymap in pairs(keymaps) do
+      for key, command in pairs(keymap) do
+        vim.keymap.set(mode, key, command, { buffer = args.buf })
+      end
+    end
+
+    -- 定義をジャンプせずにチラッと確認したい時に使う
+    vim.keymap.set("n", "gp", function()
+      local params = vim.lsp.util.make_position_params(0, 'utf-8')
+      return vim.lsp.buf_request(0, "textDocument/definition", params, function(_, result)
+        if result == nil or vim.tbl_isempty(result) then
+          print("No definition found")
+          return
+        end
+        vim.lsp.util.preview_location(result[1], { border = "rounded" })
+      end)
+    end, { desc = "Peek Definition" })
+  end,
+})
